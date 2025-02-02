a35d6b5d6058971abb31c26d1f8bd375a55d77dd
MUHAMMAD HUNAIN TARIQ <157603556+hunain-tariq@users.noreply.github.com>
2025-02-02 12:37:17 +0500

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,10 +7,12 @@ export default function Alert(props) {
     }
   return (
     <div>
+      <div style={{height: '50px'}}> 
        {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>}
     </div>
+</div>
   )
 }
